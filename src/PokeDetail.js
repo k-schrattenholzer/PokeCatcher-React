b037debf225a94701817992de8627411da019c38
‘Katie <ktschr11@gmail.comgit config --global user.name k-schrattenholzergit config --global user.email ktschr11@gmail.com>
2021-09-27 13:31:25 -0700

    able to access match.params in detail page

--- src/PokeDetail.js
+++ src/PokeDetail.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 
 export default class PokeDetail extends Component {
     render() {
+        console.log('POKE ID IS >>>', this.props.match.params.PokemonId)
         return (
             <div>
                 This is the detail page
