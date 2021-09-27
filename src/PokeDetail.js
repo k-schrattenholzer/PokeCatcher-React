6167c2f7c77127a460e80e75dd9b3afa3e767c88
‘Katie <ktschr11@gmail.comgit config --global user.name k-schrattenholzergit config --global user.email ktschr11@gmail.com>
2021-09-27 14:23:35 -0700

    working links on gallery page to detail page

--- src/PokeDetail.js
+++ src/PokeDetail.js
@@ -1,12 +1,37 @@
 import React, { Component } from 'react'
+import request from 'superagent'
+import PokeItem from './components/PokeItem.js'
 
 export default class PokeDetail extends Component {
+
+    state = {
+        pokemon: [],
+    }
+
+    componentDidMount = async() => {
+            const response = await request.get(`https://pokedex-alchemy.herokuapp.com/api/pokedex/${this.props.match.params.PokemonId}`);
+
+            this.setState({
+                pokemonData: response.body,
+            })
+            console.log(response)
+    }
+
+    
     render() {
-        console.log('POKE ID IS >>>', this.props.match.params.PokemonId)
+
         return (
             <div>
-                This is the detail page
+                <h3>This is the detail page for pokemon ID: {this.props.match.params.PokemonId}</h3>
+                {
+                    this.state.pokemon
+                    .map( pokebaby => 
+
+                        <PokeItem {...pokebaby} 
+                        key={pokebaby._id} />)
+                }
             </div>
         )
     }
 }
+
